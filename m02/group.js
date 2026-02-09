1d0f9023e85fff4cc922bd3834f189fd9db52496
sanjidaRimi023 <sanjidarimi023@gmail.com>
2026-02-09 19:07:57 +0600

    denormalized the obj data

--- m02/group.js
+++ m02/group.js
@@ -18,14 +18,45 @@ const productSales = [
 //3. if exist then increse values
 //4. or inial value 1
 
-const productCount = productSales.reduce((table, product)=>{
-  console.log(table, "" , product)
-  if(table[product]){
-    table[product]++
+const productCount = productSales.reduce((table, product) => {
+  // console.log(table, "" , product)
+  if (table[product]) {
+    table[product]++;
+  } else {
+    table[product] = +1;
   }
-  else{
-    table[product]=+1
+  return table;
+}, {});
+// console.log(productCount)
+
+// Scenario: You have a flat array of fruits data, and you need to group the fruits by category,
+// calculating the total price and the number of items sold for each.
+const fruitSales = [
+  { category: "Pome", item: "Apple", price: 44, quantity: 2 },
+  { category: "Berry", item: "Banana", price: 11, quantity: 1 },
+  { category: "Citrus", item: "Orange", price: 5, quantity: 2 },
+  { category: "Tropical", item: "Mango", price: 38, quantity: 3 },
+  { category: "Berry", item: "Grapes", price: 80, quantity: 1 },
+  { category: "Tropical", item: "Pineapple", price: 44, quantity: 5 },
+];
+//init empty onject
+//init object category
+// colculated total price
+
+const fruitsSaleByCat = fruitSales.reduce((table, saleItem) => {
+  const { category, price, quantity } = saleItem;
+  let tableCat = table[category];
+  if (!tableCat) {
+    tableCat = {
+      totalPrice: 0,
+      totalItem: 0,
+    };
+    table[category]=tableCat
   }
-  return table
-},{})
-console.log(productCount)
+
+  tableCat.totalPrice += price * quantity;
+  tableCat.totalItem = +quantity;
+
+  return table;
+}, {});
+console.log(fruitsSaleByCat);
